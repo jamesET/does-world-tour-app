1e63e694af15bfe224175c339136062f51255f51
James Thomas <jimmy.e.thomas@gmail.com>
2016-03-12 16:34:45 -0600

    Progress bar only appears after first beer ordered

--- www/js/configuration.js
+++ www/js/configuration.js
@@ -2,7 +2,7 @@
 
  angular.module('config', [])
 
-//.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080/'})
 //.constant('ENV', {name:'development',apiEndpoint:'http://jamess-air:8080/'})
-.constant('ENV', {name:'production',apiEndpoint:'https://www.mondoes.club/'})
+//.constant('ENV', {name:'production',apiEndpoint:'https://www.mondoes.club/'})
 ;
